5e720b3e497aedc8f8bfca48603e01c77b8ab461
Lekvi <furycatt@gmail.com>
2019-10-05 19:05:07 +0300

    5.10 19.05

---
+++ tasks.js
@@ -0,0 +1,34 @@
+// var n = prompt('Введи число', '');
+// function factorial(n){
+//     if (n === 1){
+//         return 1;
+//     } 
+//     else {
+//         return n*factorial(n-1);
+//     }
+// }
+// alert('Факториал числа ' + n + ' найден рекурсивным методом и равен ' + factorial(n));
+
+// function sumTo(n){
+//     if (n === 1){
+//        return 1;
+//     }
+//     else {
+//         return n + sumTo(n-1);
+//     }
+// }
+// alert('Сумма всех цифр до числа ' + n + ' найдена рекурсивным методом и равна ' + sumTo(n));
+
+// function sumToo(n){
+//     if (n === 1){
+//        return 1;
+//     }
+//     else {
+//         for (i=0;i<n;i++){
+//             var result = n+i;
+            
+//         }
+        
+//     }
+// }
+// alert(sumToo(n));
